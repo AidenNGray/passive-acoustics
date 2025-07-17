020581e025c038689b9df1e32c7a5923d7eefc10
AidenNGray <gray.aiden2004@gmail.com>
2025-07-16 18:38:23 -0700

    Starting statistics

---
+++ psdSegment.m
@@ -0,0 +1,7 @@
+function [psdMotor, fMotor, psdBG, fBG] = psdSegment(signal, sRate, timestamp, forward, back)
+%psdSegment Computes power spectral density of motor and background
+%   Assumes motor and background noise are sequential. Motor is 'forward'
+%   seconds after 'timestamp' and BG is 'back' seconds prior.
+
+
+end
